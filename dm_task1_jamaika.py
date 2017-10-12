f3753401bc5d0c4bb02dd4b27a963a63aa853cb2
jakub <jakubg@student.ethz.ch>
2017-10-12 16:56:37 +0200

    commiting the version that scores 100%

--- dm_task1_jamaika.py
+++ dm_task1_jamaika.py
@@ -4,13 +4,34 @@ from random import randrange
 
 dict_length = 8192
 num_hashes = 1024
-num_per_band = 32
+num_per_band = 16
+similarity_threshold = 0.85
 
 seed(124)   # 124 0.896
 
 # a_hash and b_hash cannot be generated on the fly if running in a distributed env. they should be same across all nodes
 a_hash = [randrange(sys.maxint) for _ in xrange(0, num_hashes)]
 b_hash = [randrange(sys.maxint) for _ in xrange(0, num_hashes)]
+
+
+def jaccard_similarity_above_threshold(set_a, set_b):
+    jaccard_similarity = float(len(set_a & set_b)) / float(len(set_a | set_b))
+    return jaccard_similarity >= similarity_threshold
+
+
+def min_hash_fn(a, b, shingles):
+    hashes = [((a * x) + b) % dict_length for x in shingles]
+    return min(hashes)
+
+
+def get_min_hash_row(shingles):
+    hashes = [min_hash_fn(a, b, shingles) for a, b in zip(a_hash, b_hash)]
+    return hashes
+
+
+def get_band(l, n):
+    for i in xrange(0, len(l), n):
+        yield frozenset(l[i:i + n])
 
 
 def mapper(key, value):
@@ -22,28 +43,12 @@ def mapper(key, value):
     shingles = [int(numeric_string) for numeric_string in shingles]     # cast string shingles to int
     shingles = sorted(shingles)
 
-
-    def min_hash_fn(a, b, sig):
-        hashes = [((a * x) + b) % dict_length for x in sig]
-        return min(hashes)
-
-    def get_min_hash_row(sig):
-        hashes = [min_hash_fn(a, b, sig) for a, b in zip(a_hash, b_hash)]
-        return hashes
-
-    def get_band(l, n):
-        for i in xrange(0, len(l), n):
-            yield frozenset(l[i:i + n])
-
-
-    signature = shingles
-    min_hash_row = get_min_hash_row(signature)
-    #print min_hash_row
+    min_hash_row = get_min_hash_row(shingles)
 
     banded = get_band(min_hash_row, num_per_band)
 
     for band in banded:
-        yield hash(band), page_id
+        yield hash(band), (page_id, shingles)
 
 
 def reducer(key, values):
@@ -51,9 +56,9 @@ def reducer(key, values):
     # values: list of all value for that key
 
     if len(values) > 1: # if there is a collision in the bucket
-        values = sorted(values)
-        for i in range(0,len(values)):
-            for j in range(i+1, len(values)):
-                print values[i], values[j]
-                yield values[i], values[j]
+        values.sort()
+        for i in range(0, len(values)):
+            for j in range(i + 1, len(values)):
+                if jaccard_similarity_above_threshold(set(values[i][1]), set(values[j][1])):
+                    yield values[i][0], values[j][0]
 
